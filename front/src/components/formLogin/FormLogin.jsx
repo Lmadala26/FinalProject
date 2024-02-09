b1fea67d3d96170e4cea036f4b2d5424b9a2a3c1
Roman <sldoxik@hotmail.com>
2024-02-09 12:54:12 +0100

    subido css del login y el singup

--- front/src/components/formLogin/FormLogin.jsx
+++ front/src/components/formLogin/FormLogin.jsx
@@ -3,6 +3,8 @@ import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContextProvider";
 import { loginUserService } from "../../service/loginUserService";
 
+import styles from './formLogin.module.css'
+
 const FormLogin = () => {
   const [email, setEmail] = useState("");
   const [passwd, setPasswd] = useState("");
@@ -28,34 +30,36 @@ const FormLogin = () => {
 
   return (
     <>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">Email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setPasswd(e.target.value)}
-          />
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
-        <div>
-          <Link to={"/user/recover-password"}>
-            <p>Recupar contraseña</p>
-          </Link>
-        </div>
-        {error ? <p>{error}</p> : null}
-      </form>
+      <section className={styles.DadLogin}>
+        <h2 className={styles.loginTitle}>Login</h2>
+        <form className={styles.dadForm} onSubmit={handleSubmit}>
+          <div className={styles.inputs}>
+            <label htmlFor="">Email</label>
+            <input
+              className={styles.inputEmail}
+              type="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="">Password</label>
+            <input
+              className={styles.inputPassword}
+              type="password"
+              name="password"
+              onChange={(e) => setPasswd(e.target.value)}
+            />
+            <Link to={"/user/recover-password"}>
+              <p className={styles.linkRecover}>Recupar contraseña</p>
+            </Link>
+          </div>
+          <div>
+              
+            <button className={styles.botonLogin}>Login</button>
+          </div>
+
+          {error ? <p className={styles.errorStyle}>{error}</p> : null}
+        </form>
+      </section>
     </>
   );
 };
--- front/src/pages/LogInPage.jsx
+++ front/src/pages/LogInPage.jsx
@@ -2,9 +2,6 @@ import FormLogin from "../components/formLogin/FormLogin";
 
 export const LoginPage = () => {
   return (
-    <>
-      <h1 style={{ color: "yellow" }}>Login</h1>
       <FormLogin />
-    </>
   );
 };
