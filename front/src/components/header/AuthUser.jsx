4f00f53e9b005d57aa29a1ba1bb13ce4b1685a84
Lucas Madala <lu.madala_26@hotmail.com>
2024-02-11 23:20:58 +0100

    AuthUser

---
+++ front/src/components/header/AuthUser.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContextProvider";
+import loggedUserService from "../../service/loggedUserService";
+import styles from "./logged.module.css";
+import userIcon from "../../assets/userIcon.jpg";
+
+export const AuthUser = () => {
+  const { token } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+  const logout = () => {
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const data = await loggedUserService({ token });
+        setUser(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchUserData();
+  }, [token]);
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <span>{user.email}</span>
+          <Link to={"/users"}>
+            <img
+              src={
+                user.profilePhoto
+                  ? `${VITE_API_URL}/uploads/${user.profilePhoto}`
+                  : userIcon
+              }
+              alt="imagen"
+              width={"50px"}
+              height={"50px"}
+            />
+          </Link>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <div className={styles.Buttons}>
+          <Link to={"/users/register"}>
+            <button> Sign In</button>
+          </Link>
+          <Link to={"/users/login"}>
+            <button> Log in</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
